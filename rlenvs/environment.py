6680857c0465a9e5bdb09677cb9b1d3e551dead5
Jordan Bishop <jordanbishop26@gmail.com>
2021-10-05 15:11:12 +1000

    Add env iod rng reseeding method

--- rlenvs/environment.py
+++ rlenvs/environment.py
@@ -186,6 +186,9 @@ class EnvironmentABC(metaclass=abc.ABCMeta):
     def is_terminal(self):
         return self._is_terminal
 
+    def reseed_iod_rng(self, new_seed):
+        self._iod_rng = self._make_iod_rng(new_seed)
+
     def render(self):
         self._wrapped_env.render()
 
